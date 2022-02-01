b534d83eb19cebcc9f10e4175170b3ecc07c659c
pixls <tha.pixls@gmail.com>
2022-02-01 11:12:45 -0500

    added first question page

--- app.py
+++ app.py
@@ -6,9 +6,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "just-my-secret"
 
 responses = []
+title=satisfaction_survey.title
+instructions=satisfaction_survey.instructions
+current_q = 0
 
 @app.route('/')
 def survey_start():
 
+    return render_template('survey.html', title=title, instructions=instructions)
 
-    return render_template('survey.html', title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
+@app.route('/questions/0')
+def question_0():
+    question = satisfaction_survey.questions[0].question
+    choices = satisfaction_survey.questions[0].choices
+
+    return render_template('question_0.html', question = question, choices = choices, title=title )
+
+@app.route('/answers', methods=["POST"])
+def answers():
+    
