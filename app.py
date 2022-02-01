df6e6e450a619778a4f0137db95f927366baf8a0
pixls <tha.pixls@gmail.com>
2022-02-01 12:18:35 -0500

    questions work, added styling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -15,13 +15,28 @@ def survey_start():
 
     return render_template('survey.html', title=title, instructions=instructions)
 
-@app.route('/questions/0')
-def question_0():
-    question = satisfaction_survey.questions[0].question
-    choices = satisfaction_survey.questions[0].choices
-
-    return render_template('question_0.html', question = question, choices = choices, title=title )
-
 @app.route('/answers', methods=["POST"])
 def answers():
+    global current_q
+    global responses
+    answer = request.form[f"q{current_q}"]
+
+    responses.append(answer)
     
+    current_q += 1
+
+    return redirect(f"/questions/{current_q}")
+
+@app.route('/questions/<int:q_num>')
+def question(q_num):
+    if q_num == current_q:
+        if q_num >= len(satisfaction_survey.questions):
+            return render_template('thanks.html', title=title)
+        else:
+            question = satisfaction_survey.questions[q_num].question
+            choices = satisfaction_survey.questions[q_num].choices
+
+            return render_template('question.html', question = question, choices = choices, title=title, q_num=q_num )
+    else:
+        flash("you were trying to visit the wrong page, now you're back where you should be", "warning")
+        return redirect(f"/questions/{current_q}")
